55d9fc1b2c5941de9313a7c4f73474e59586f23a
samyukta-2 <samyuktar48@gmail.com>
2025-05-26 21:33:10 +0530

    added few lines

--- story.py
+++ story.py
@@ -1,3 +1,4 @@
 import random
 
 characters = ["a brave knight", "a clever princess", "a mischievous dragon", "an adventurous robot"]
+settings = ["in a haunted castle", "on a distant planet", "in a magical forest", "at the edge of a volcano"]
